da17761b63b199ae2830797ca4332078620cbf47
Léon Hollender <hello@leonh.fr>
2024-10-05 20:47:05 +0200

    feat: run action

--- cmd/github-action/main.go
+++ cmd/github-action/main.go
@@ -2,7 +2,16 @@ package main
 
 import "github.com/sethvargo/go-githubactions"
 
+const apiTokenInput = "api_token"
+
 func main() {
 	gha := githubactions.New()
+
+	token := gha.GetInput(apiTokenInput)
+	if token == "" {
+		gha.Fatalf("%s required", apiTokenInput)
+	}
+
 	gha.Infof("Hello, world!")
+	gha.Infof("%s=%s", apiTokenInput, token)
 }
