29716c0831da2c438f86d629cbd34a8111a978c3
Léon Hollender <hello@leonh.fr>
2024-10-08 00:30:27 +0200

    feat: rename files

--- internal/worker/card.go
+++ internal/worker/card.go
@@ -3,6 +3,7 @@ package worker
 import (
 	"context"
 
+	"github.com/leonhfr/mochi/internal/lock"
 	"github.com/leonhfr/mochi/mochi"
 )
 
@@ -35,6 +36,32 @@ func FetchCards(ctx context.Context, logger Logger, client *mochi.Client, in <-c
 				},
 				err: err,
 			}
+		}
+	}()
+	return out
+}
+
+// CleanedCards contains a deck with existing cards.
+type CleanedCards struct {
+	deckID    string
+	filePaths []string
+	cards     []mochi.Card
+}
+
+// CleanCards cleans the lockfile from the cards that have been removed.
+func CleanCards(logger Logger, lf *lock.Lock, in <-chan ExistingCards) <-chan CleanedCards {
+	out := make(chan CleanedCards)
+	go func() {
+		defer close(out)
+
+		for existingCards := range in {
+			logger.Infof("cleaning cards for deck %s", existingCards.deckID)
+			var cardIDs []string
+			for _, card := range existingCards.cards {
+				cardIDs = append(cardIDs, card.ID)
+			}
+			lf.CleanCards(existingCards.deckID, cardIDs)
+			out <- CleanedCards(existingCards)
 		}
 	}()
 	return out
--- internal/worker/deck.go
+++ internal/worker/deck.go
@@ -1,8 +1,12 @@
 package worker
 
 import (
+	"context"
+
 	"github.com/leonhfr/mochi/internal/config"
 	"github.com/leonhfr/mochi/internal/deck"
+	"github.com/leonhfr/mochi/internal/lock"
+	"github.com/leonhfr/mochi/mochi"
 )
 
 // FilteredDeck represents a deck whose config has been matched.
@@ -27,6 +31,30 @@ func FilterDeck(logger Logger, cfg *config.Config, in <-chan deck.Directory) <-c
 				}
 			} else {
 				logger.Infof("deck filter: discarded %s with %d files", dir.Path, len(dir.FilePaths))
+			}
+		}
+	}()
+	return out
+}
+
+// SyncedDeck contains a synced deck.
+type SyncedDeck struct {
+	id        string
+	filePaths []string
+}
+
+// SyncDeck creates any missing decks and updates any mismatched name.
+func SyncDeck(ctx context.Context, logger Logger, client *mochi.Client, cfg *config.Config, lf *lock.Lock, in <-chan FilteredDeck) <-chan Result[SyncedDeck] {
+	out := make(chan Result[SyncedDeck])
+	go func() {
+		defer close(out)
+
+		for filteredDeck := range in {
+			logger.Infof("deck sync: %s", filteredDeck.path)
+			deckID, err := deck.Sync(ctx, client, cfg, lf, filteredDeck.path)
+			out <- Result[SyncedDeck]{
+				data: SyncedDeck{id: deckID, filePaths: filteredDeck.filePaths},
+				err:  err,
 			}
 		}
 	}()
